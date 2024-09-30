c235e8fa0d2c1bbfb97d43c27c8a044213fba9a2
mohamedthowfiq <mohamedthowfiq9626@gmail.com>
2024-09-30 18:09:09 +0530

    created views and url and session and carts.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,8 +7,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # Admin url
     path('admin/', admin.site.urls),
+
+    # Store app
     path('',include('store.urls')),
+
+    # Cart app
+    path('cart/',include('cart.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
